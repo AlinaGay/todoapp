d2731621b42b60f3a05abd493354a407adc3eb6b
AlinaGay <gayfullina.a@gmail.com>
2023-12-12 15:39:38 +0100

    added new pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
-  root "application#hello"
+  # root "application#hello"
+  root "pages#home"
+  get '/about', to: 'pages#about'
+  get '/help-page', to: 'pages#help_page'
   # Defines the root path route ("/")
   # root "posts#index"
 end
